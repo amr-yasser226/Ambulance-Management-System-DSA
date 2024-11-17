9ab61e587052028dfb89d24cf8a94742422aae9d
Momen <s-momen.sanad@zewailcity.edu.eg>
2024-11-17 16:36:53 +0200

    minor changes @ organizer.h

--- src/Organizer/organizer.h
+++ src/Organizer/organizer.h
@@ -1,21 +1,23 @@
 #ifndef ORGANIZER_H
 #define ORGANIZER_H
 
+#define SIZE 10             // size of array of pointers to hosiptal objects
 #include "hospital.h"
 
 class Organizer
 {
 private:
-    Hospital *hospitals[10]; // Fixed-size array of pointers to Hospital objects (adjust size as needed)
+
+    Hospital** hospitals= new Hospital*[SIZE]; // Fixed-size array of pointers to Hospital objects (adjust size as needed)
     int hospitalCount;
     int currentTime;
 
 public:
     Organizer();
-    ~Organizer();
     void loadInputData();
     void simulate();
     void generateOutput();
+    ~Organizer();
 };
 
 #endif // ORGANIZER_H
